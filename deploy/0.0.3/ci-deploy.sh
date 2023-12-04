f084120bfd2bddca33fed1d91049ee0aafbab4ec
Elena Torró <elena@tinybird.co>
2023-12-04 08:57:00 +0100

    Add release 0.0.3

--- deploy/0.0.3/ci-deploy.sh
+++ deploy/0.0.3/ci-deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 
-tb datasource rm day_3 --yes
-tb push datasources/day_3.datasource --force --fixtures
-tb deploy --fixtures --wait --yes
+tb deploy --populate --fixtures --wait
